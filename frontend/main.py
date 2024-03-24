cb053b90b2d0febb7ac48aa46773ffa0f48ef5e9
DevavratSinghBisht <devavratsinghbisht@gmail.com>
2024-03-24 22:41:22 +0530

    code refactor

--- frontend/main.py
+++ frontend/main.py
@@ -7,18 +7,25 @@ from config import BACKED_URL
 with st.sidebar:
     st.title('Chatbot')
 
-# Store LLM generated responses
+# Write a message if none exists
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
-# Display or clear chat messages
+# Display all messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
+# Clear all messages
 def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Take and display user prompt
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
 # Function for calling backend
 def get_response(prompt_input):
@@ -28,22 +35,11 @@ def get_response(prompt_input):
     print(response)
     return response['response']
 
-# User-provided prompt
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = get_response(prompt)
-            placeholder = st.empty()
-            full_response = ''
-            for item in response:
-                full_response += item
-                placeholder.markdown(full_response)
-            placeholder.markdown(full_response)
-    message = {"role": "assistant", "content": full_response}
+        st.write(response)
+    message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
